7fc507d59023cd6cc712abb694ba43b656afa6d8
lisazacarias <zacarias@slac.stanford.edu>
2019-02-25 18:33:45 -0500

    Final Results
    Could still use some cleaning

--- calibration.py
+++ calibration.py
@@ -1,58 +1,142 @@
 from csv import reader
 from datetime import datetime
 from matplotlib import pyplot as plt
-from numpy import mean, std
+from numpy import mean, std, polyfit
 from scipy.stats import ttest_ind
+from sys import maxint
 
-CUTOFF = 500
+CRYOMODULE = "2"
+VALVE_LOCKED_POS = 17
 
 time = []
 unixTime = []
 valvePos = []
 flowRate = []
 heaterPower = []
+downstreamLevel = []
+upstreamLevel = []
 
 def checkAndAppend(dataBuffers, startIdx, endIdx):
     for (runBuffer, dataBuffer) in dataBuffers:
         if endIdx - startIdx > 0:
             runBuffer.append(dataBuffer[startIdx : endIdx])
 
-with open("data.csv") as csvfile:
-    csvReader = reader(csvfile)
-    header = csvReader.next()
-    for row in csvReader:
-        time.append(datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S"))
-        unixTime.append(float(row[1]))
-        valvePos.append(float(row[2]))
-        flowRate.append(float(row[3]))
-        heaterPower.append(float(row[4]))
+def genString(prefix, suffix):
+    return prefix + CRYOMODULE + suffix
 
-currVal = heaterPower[0]
-runs = []
-timeRuns = []
-currIdx = 0
+def parseData(fileName):
+    with open(fileName) as csvfile:
+        csvReader = reader(csvfile)
+        header = csvReader.next()
+        
+        for row in csvReader:
+        
+            def appendToBuffer(buff, columnName):
+                buff.append(float(row[header.index(columnName)]))
+                
+            time.append(datetime.strptime(row[header.index("time")],
+                                          "%Y-%m-%d %H:%M:%S"))
+                                          
+            for buff, col in [(unixTime, "Unix time"),
+                              (valvePos, genString("CPV:CM0", ":3001:JT:POS_RBV")),
+                              #(flowRate, "CFICM0312"),
+                              (heaterPower, genString("CHTR:CM0", ":1155:HV:POWER")),
+                              (downstreamLevel, genString("CLL:CM0", ":2301:DS:LVL")),
+                              (upstreamLevel, genString("CLL:CM0", ":2601:US:LVL"))]:
+                              
+                appendToBuffer(buff, col)
 
-for idx, val in enumerate(heaterPower):
-    if val != currVal:
-        # Discarding the first CUTOFF points where the value is settling
-        startIdx = currIdx + CUTOFF
-        checkAndAppend([(runs, flowRate), (timeRuns, unixTime)], startIdx, idx)
-        currIdx = idx
-    currVal = val
+def getLiquidLevelChange():
+    parseData("LL_test_cropped.csv")
+    
+    runs, timeRuns, heaterVals = populateRuns(heaterPower, downstreamLevel, 66,
+                                 1.2)
+                                 
+    print heaterVals
+    
+    ax1 = genAxis("Liquid Level as a Function of Time", "Unix Time (s)",
+              "Downstream Liquid Level (%)")
+              
+    ax2 = genAxis("Rate of Change of Liquid Level as a Function of Heater Power",
+                  "Heater Power (W)", "dLL/dt (%/s)")
+    
+    # Needed as a consequence of using scatterplot - should probably not hard
+    # code these
+    ax2.set_ylim([-0.002, 0])
 
-startIdx = currIdx + CUTOFF
-checkAndAppend([(runs, flowRate), (timeRuns, unixTime)], startIdx,
-               len(heaterPower) - 1)
+    slopes = []
 
-for idx, run in enumerate(runs):
-    print str(run[0:10]) + " TRUNCATED FROM " + str(len(run))
-    ave = mean(run)
-    print std(run)
-    plt.plot(timeRuns[idx], run)
-    plt.plot(timeRuns[idx], [ave for _ in timeRuns[idx]])
+    for idx, run in enumerate(runs):
+        m, b = polyfit(timeRuns[idx], run, 1)
+        slopes.append(m)
+        ax1.plot(timeRuns[idx], run, label=(str(round(m, 6)) + "%/s @ "
+                                            + str(heaterVals[idx]) + " W"))
+        ax1.plot(timeRuns[idx], [m*x + b for x in timeRuns[idx]])
+        
+    ax2.scatter(heaterVals, slopes, marker="o")
+    m, b = polyfit(heaterVals, slopes, 1)
+    ax2.plot(heaterVals, [m*x + b for x in heaterVals], label=(str(m)+" %/(s*W)"))
 
-print ttest_ind(runs[-1][0:len(runs[-1])/2], runs[-1][len(runs[-1])/2:-1])
+    ax1.legend(loc='lower right')
+    ax2.legend(loc='upper right')
+    
+    plt.show()
+    
+def getAverage():
+    parseData("data_new.csv")
+    
+    runs, timeRuns, heaterVals = populateRuns(heaterPower, flowRate, 0, maxint)
+    print heaterVals
+    
+    for idx, run in enumerate(runs):
+        print str(run[0:10]) + " TRUNCATED FROM " + str(len(run))
+        ave = mean(run)
+        print ave
+        print std(run)
+        plt.plot(timeRuns[idx], run)
+        plt.plot(timeRuns[idx], [ave for _ in timeRuns[idx]])
+        
+    plt.show()
 
-print ttest_ind(runs[0], runs[-1])
+def populateRuns(inputBuffer, outputBuffer, levelLimit, valvePosLimit):
+    currVal = inputBuffer[0]
+    runs = []
+    timeRuns = []
+    currIdx = 0
+    inputVals = []
+    
+    for idx, val in enumerate(inputBuffer):
+            
+        # A "break" condition defining the end of a run
+        if (val != currVal or upstreamLevel[idx] < levelLimit
+            or abs(valvePos[idx] - VALVE_LOCKED_POS) > valvePosLimit):
 
-plt.show()
+            # Discarding the first CUTOFF points where the value is settling
+            if idx - currIdx > 1000:
+                cutoff = int(abs(val - currVal) * 55)
+                inputVals.append(currVal)
+                print "cutoff: " + str(cutoff)
+                checkAndAppend([(runs, outputBuffer), (timeRuns, unixTime)],
+                               currIdx + cutoff, idx)
+            
+            currIdx = idx
+            
+        currVal = val
+    
+    if len(inputBuffer) - currIdx > 1000:
+        inputVals.append(inputBuffer[len(inputBuffer) - 1])
+        checkAndAppend([(runs, outputBuffer), (timeRuns, unixTime)], currIdx,
+                       len(inputBuffer))
+               
+    return runs, timeRuns, inputVals
+
+def genAxis(title, xlabel, ylabel):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return ax
+
+getLiquidLevelChange()
+#getAverage()
