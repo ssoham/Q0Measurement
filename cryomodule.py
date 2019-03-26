83aaf067fe613f39434fd3b7aa8a2ccf43237a3a
lisazacarias <zacarias@slac.stanford.edu>
2019-03-26 09:45:12 -0400

    Introduced Object Orientation, added capacity for user interaction, lots of comments (#10)
    * first pass at making it OO successful - still have some cleaning to do
    
    * wrote cavity logic, but it's borked
    
    * cosmetic changes, added gradient PV
    
    * adding comments
    
    * overhauled and commented everything. seems to be working...?
    
    *  Addressing comments on pull request 10 (#2)
    
    * Clarifying comment & check to avoid overwriting files in generateCSV
    
    * Fixed python version compatibility
    
    * Removed python 3 shebang from the top of the file
    
    * Added cryomodule and cavity classes
    
    * Addressing comments on pull request 10
    
    * Addressing comments on pull request 2
    
    * started UI file

--- cryomodule.py
+++ cryomodule.py
@@ -1,44 +1,113 @@
+################################################################################
+# Utility classes to hold calibration data for a cryomodule, and Q0 measurement
+# data for each of that cryomodule's cavities
+# Authors: Lisa Zacarias, Ben Ripman
+################################################################################
+
+
 class Cryomodule:
+    # We're assuming that the convention is going to be to run the calibration
+    # on cavity 1, but we're leaving some wiggle room in case
+    def __init__(self, cryModNumSLAC, cryModNumJLAB, calFileName,
+                 refValvePos, refHeaterVal, calCavNum=1):
 
-    class Cavity:
+        self.cryModNumSLAC = cryModNumSLAC
+        self.cryModNumJLAB = cryModNumJLAB
+        self.dataFileName = calFileName
+        self.refValvePos = refValvePos
+        self.refHeaterVal = refHeaterVal
+        self.calCavNum = calCavNum
 
-        def __init__(self, _parent, cavNumber):
-            self.parent = _parent
-            self.number = cavNumber
-            
-            self.heaterPV = ("CHTR:CM0" + str(_parent.jlabNum) + ":1"
-                             + str(cavNumber) + "55:HV:POWER")
-            
-            self.q0MeasTime = []
-            self.q0MeasUnixTime = []
-            self.q0MeasValvePos = []
-            self.q0MeasHeatLoad = []
-            self.q0MeasDownstreamLevel = []
-            self.q0MeasUpstreamLevel = []
-    
-    def __init__(self, slacNumber, jlabNumber):
-        self.slacNum = slacNumber
-        self.jlabNum = jlabNumber
-        self.cavities = [self.Cavity(self, i) for i in xrange(1, 9)]
-
-        jlabNumStr = str(self.jlabNum)
+        jlabNumStr = str(self.cryModNumJLAB)
         self.valvePV = "CPV:CM0" + jlabNumStr + ":3001:JT:POS_RBV"
-        self.massFlowPV = "CFICM0312"
         self.dsLevelPV = "CLL:CM0" + jlabNumStr + ":2301:DS:LVL"
         self.usLevelPV = "CLL:CM0" + jlabNumStr + ":2601:US:LVL"
 
-        self.calTime = []
-        self.calUnixTime = []
-        self.calValvePos = []
-        self.calFlowRate = []
-        self.calHeatLoad = []
-        self.calDownstreamLevel = []
-        self.calUpstreamLevel = []
-        
+        # These buffers store calibration data read from the CSV dataFileName
+        self.unixTimeBuffer = []
+        self.timeBuffer = []
+        self.valvePosBuffer = []
+        self.heatLoadBuffer = []
+        self.downstreamLevelBuffer = []
+        self.upstreamLevelBuffer = []
+
+        # Maps this cryomodule's PV's to its corresponding data buffers
+        self.pvBufferMap = {self.valvePV: self.valvePosBuffer,
+                            self.dsLevelPV: self.downstreamLevelBuffer,
+                            self.usLevelPV: self.upstreamLevelBuffer}
+
+        # Give each cryomodule 8 cavities
+        self.cavities = {i: self.Cavity(parent=self, cavNumber=i)
+                         for i in xrange(1, 9)}
+
+    # Returns a list of the PVs used for its data acquisition, including
+    # the PV of the cavity heater used for calibration
+    def getPVs(self):
+        return [self.valvePV, self.dsLevelPV, self.usLevelPV,
+                self.cavities[self.calCavNum].heaterPV]
+
+    class Cavity:
+        def __init__(self, parent, cavNumber, q0MeasFileName=""):
+            self.parent = parent
+
+            self.cavityNumber = cavNumber
+            self.dataFileName = q0MeasFileName
+
+            heaterPVStr = "CHTR:CM0{cryModNum}:1{cavNum}55:HV:POWER"
+            self.heaterPV = heaterPVStr.format(cryModNum=parent.cryModNumJLAB,
+                                               cavNum=cavNumber)
+
+            gradientPVStr = "ACCL:L1B:0{cryModNum}{cavNum}0:GACT"
+            self.gradientPV = gradientPVStr.format(cryModNum=parent.cryModNumJLAB,
+                                                   cavNum=cavNumber)
+
+            # These buffers store Q0 measurement data read from the CSV
+            # dataFileName
+            self.unixTimeBuffer = []
+            self.timeBuffer = []
+            self.valvePosBuffer = []
+            self.heatLoadBuffer = []
+            self.downstreamLevelBuffer = []
+            self.upstreamLevelBuffer = []
+            self.gradientBuffer = []
+
+            # Maps this cavity's PVs to its corresponding data buffers
+            # (including a couple of PVs from its parent cryomodule)
+            self.pvBufferMap = {self.parent.valvePV: self.valvePosBuffer,
+                                self.parent.dsLevelPV:
+                                    self.downstreamLevelBuffer,
+                                self.parent.usLevelPV: self.upstreamLevelBuffer,
+                                self.heaterPV: self.heatLoadBuffer,
+                                self.gradientPV: self.gradientBuffer}
+
+        # Similar to the Cryomodule function, it just has the gradient PV
+        # instead of the heater one
+        def getPVs(self):
+            return [self.parent.valvePV, self.parent.dsLevelPV,
+                    self.parent.usLevelPV, self.gradientPV]
+
+        # The @property annotation is effectively a shortcut for defining a
+        # class variable and giving it a custom getter function (so now
+        # whenever someone calls Cavity.refValvePos, it'll return the parent
+        # value)
+        @property
+        def refValvePos(self):
+            return self.parent.refValvePos
+
+        @property
+        def refHeaterVal(self):
+            return self.parent.refHeaterVal
+
+        @property
+        def cryModNumSLAC(self):
+            return self.parent.cryModNumSLAC
+
 
 def main():
-    derp = Cryomodule(12, 2)
-    for cav in derp.cavities:
+    cryomodule = Cryomodule(cryModNumSLAC=12, cryModNumJLAB=2, calFileName="",
+                            refValvePos=0, refHeaterVal=0)
+    for idx, cav in cryomodule.cavities.iteritems():
+        print cav.gradientPV
         print cav.heaterPV
 
 
--- user_input.py
+++ user_input.py
@@ -1,34 +1,35 @@
 """user_input.py -- Contains the get_input function. Has debugging
 functionality if called as __main__.
+Author: Ben Ripman
+Editor: Lisa Zacarias
 """
+
+ERROR_MESSAGE = "Please provide valid input"
 
 
 def get_input(prompt, desired_type):
     """Uses the supplied prompt to pester the user until they give
     input of the desired type.
     """
-    acceptable_input = False
-    while not acceptable_input:
-        response = raw_input(prompt)
-        if not response:
-            print("You've gotta give me SOMETHING!")
-            continue
-        if desired_type == str:
-            try:
-                response = str(response)
-            except ValueError:
-                print("Please enter a string!")
-                continue
-            acceptable_input = True
-        elif desired_type == int:
-            try:
-                response = int(response)
-            except ValueError:
-                print("Please enter an integer!")
-                continue
-            acceptable_input = True
-        else:
-            raise TypeError('Unsupported input type requested.')
+
+    response = raw_input(prompt)
+
+    try:
+        response = desired_type(response)
+    except ValueError:
+        print ERROR_MESSAGE
+        return get_input(prompt, desired_type)
+
+    return response
+
+
+def get_str_limited(prompt, acceptable_strings=[]):
+
+    response = get_input(prompt, str)
+    while response not in acceptable_strings:
+        print ERROR_MESSAGE
+        response = get_input(prompt, str)
+
     return response
 
 
@@ -37,13 +38,20 @@ def get_str(prompt, constrained=False, acceptable_strings=[]):
     string. If the constrained arg is True, only accepts input that is
     contained within the acceptable_strings list.
     """
-    acceptable_input = False
-    while not acceptable_input:
-        response = get_input(prompt, str)
-        if constrained and response not in acceptable_strings:
-            print("That is not an acceptable answer.")
-        else:
-            acceptable_input = True
+
+    if constrained:
+        return get_str_limited(prompt, acceptable_strings)
+    else:
+        return get_input(prompt, str)
+
+
+def get_int_limited(prompt, low_lim, high_lim):
+    response = get_input(prompt, int)
+
+    while response < low_lim or response > high_lim:
+        print ERROR_MESSAGE
+        response = get_input(prompt, int)
+
     return response
 
 
@@ -52,14 +60,33 @@ def get_int(prompt, constrained=False, low_lim=0, high_lim=1):
     integer. If the constrained arg is True, only accepts input that is
     >= low_lim and <= high_lim.
     """
-    acceptable_input = False
-    while not acceptable_input:
-        response = get_input(prompt, int)
-        if constrained and (response < low_lim or response > high_lim):
-            print("That is not an acceptable answer.")
-        else:
-            acceptable_input = True
+
+    if constrained:
+        return get_int_limited(prompt, low_lim, high_lim)
+    else:
+        return get_input(prompt, int)
+
+
+def get_float_limited(prompt, low_lim, high_lim):
+    response = get_input(prompt, float)
+
+    while response < low_lim or response > high_lim:
+        print ERROR_MESSAGE
+        response = get_input(prompt, float)
+
     return response
+
+
+def get_float(prompt, constrained=False, low_lim=0.0, high_lim=1.0):
+    """Uses the supplied prompt to pester the user until they yield a
+    float. If the constrained arg is True, only accepts input that is
+    >= low_lim and <= high_lim.
+    """
+
+    if constrained:
+        return get_float_limited(prompt, low_lim, high_lim)
+    else:
+        return get_input(prompt, float)
 
 
 def main():
@@ -67,12 +94,12 @@ def main():
     print("\nHello world from user_input.py!\n")
 
     print("Let's try asking the user for some input.")
-    str_input1 = get_str('Give me any string: ')
-    str_input2 = get_str('Give me a decision (y or n): ', True, ['y', 'n'])
-    int_input1 = get_int('Give me any integer: ')
-    int_input2 = get_int('Give me a number between 1 and 10: ',
-                             True, 1, 10)
-
+    get_str('Give me any string: ')
+    get_str('Give me a decision (y or n): ', True, ['y', 'n'])
+    get_int('Give me any integer: ')
+    get_int('Give me a number between 1 and 10: ', True, 1, 10)
+    get_float('Give me any float: ')
+    get_float('Give me a float between 0 and 1: ', True, 0, 1)
 
 if __name__ == '__main__':
     main()
