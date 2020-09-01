76c94f3475db547ec6e4de7ee6627d696bb96a4d
physics group account <nneveu@slac.stanford.edu>
2020-08-31 19:00:48 -0700

    everything gui is borked. gonna see if it's less borked locally

---
+++ gui/q0.py
@@ -0,0 +1,9 @@
+from pydm import Display
+from PyQt5 import QtGui
+
+class Q0(Display):
+    def __init__(self, parent=None, args=None):
+        super(Q0, self).__init__(parent=parent, args=args)
+    def ui_filename(self):
+        return "q0.ui"
+    
